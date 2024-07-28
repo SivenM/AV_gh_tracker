16ff35116f010ed5ce178d08b478d2ecee3b5a86
SivenM <aztek8877@gmail.com>
2024-07-28 19:51:07 +0300

    Добавлены фичи для модели, обновлен кэш, изменена структура данных коммита, в сообщения добавленна доп. информация о фичах.

--- commit_tracker.py
+++ commit_tracker.py
@@ -1,6 +1,8 @@
+from typing import Optional
 import os
 import yaml
 import github_api
+from pydantic import BaseModel
 import datetime
 import time
 import argparse
@@ -9,6 +11,105 @@ import pytz
 from tg_app import TgBot
 from github import Commit, PaginatedList, PullRequest
 from loguru import logger
+
+
+class CommitData(BaseModel):
+    date:str
+    author:str
+    hash:str
+    num_lines:int
+    num_files:int
+    delta_day:Optional[float]
+    delta_pl:Optional[float]
+    message:str
+    url:str
+    pl_url:str
+    
+
+class Cache:
+
+    def __init__(self, history_dir:str) -> None:
+        self.history_dir = history_dir
+        self.commits_lake = []
+        self.authors_dates = {}
+
+    def get(self, data:list) -> None:
+        if len(data) > 0:
+            for commit in data:
+                author = commit.author
+                date = datetime.datetime.fromisoformat(commit.date)
+                if author in self.authors_dates:
+                    self.authors_dates[author].append(date)
+                else:
+                    self.authors_dates[author] = [date]
+    
+                self.commits_lake.append(commit)
+
+    def save(self, date) -> None:
+        save_data = list(map(lambda x: x.model_dump(), self.commits_lake))
+        save_path = os.path.join(self.history_dir, 'commits_' + date.strftime('%d-%m-%Y') + '.json')
+        utils.save_json(save_data, save_path)
+        logger.info(f"cache saved in {save_path}")
+
+    def clear(self) -> None:
+        self.commits_lake = []
+        self.authors_dates = {}
+        logger.info("cache cleared!")
+
+    def set_author_last_date(self, author:str) -> list:
+        if author in self.authors_dates:
+            return self.authors_dates[author][-1]
+        else:
+            return None
+
+
+class FeatureCreator:
+
+    def __init__(self) -> None:
+        pass
+
+    def create_num_lines_files(self,  commit:Commit) -> int:
+        files = list(commit.files)
+        num_files = len(list)
+        count_line = 0
+        for file in files:
+            diff = file.patch
+            for diff_line in diff.splitlines():
+                    if diff_line[0] == '+' and not diff_line[:3] == "+++" and diff_line != '-':
+                        count_line += 1
+        return count_line, num_files
+
+    def _create_delta(self, curr_day:datetime.datetime, prev_commit_date:datetime.datetime) -> float:
+        return (curr_day - prev_commit_date).total_seconds()
+
+    def create_delta_day(self, commit_date:datetime.datetime, author_last_date:datetime):
+        if author_last_date == None:
+            delta_day = 0.
+        else:
+            delta_day = self._create_delta(commit_date, author_last_date)
+        return delta_day
+
+    def create_delta_pl(self, commit_date:datetime.datetime, prev_commit:datetime.datetime):
+        if prev_commit == None:
+            delta_pl == 0.
+        else:
+            delta_pl = self._create_delta(commit_date, prev_commit)
+        return delta_pl
+
+    def create_features(self, commit:Commit, prev_commit_pl:Commit=None, prev_date_author:datetime.datetime=None, pl_url:str=None) -> CommitData:
+        num_lines, num_files = self.create_num_lines_files(commit)
+        return CommitData(
+            date=commit.commit.author.date.isoformat(),
+            author=commit.commit.author.name,
+            hash=commit.commit.sha,
+            message=commit.commit.message,
+            url=commit.url,
+            pl_url=pl_url,
+            num_lines=num_lines,
+            num_files=num_files,
+            delta_day=self.create_delta_day(commit.commit.author.date, prev_date_author),
+            delta_pl=self.create_delta_pl(commit.commit.author.date, prev_commit_pl)
+        )
 
 
 class TimeHandler:
@@ -44,28 +145,37 @@ class Messanger:
 
     def __init__(self, outer=None) -> None:
         self.outer = outer
-        self.commit_info_form = "New commit:\n\n{}\n\n\tdate: {}\n\tcommit author: {}\n\tcommit hash: {}\n\n" + \
-            "[commit link]({})\n[pull request link]({})" + "_"*10 + "\n"
+        self.commit_info_form = "New commit:\n\n{}\n\n\tdate: {}\n\tcommit author: {}\n\tcommit hash: {}" + \
+            "\n\tnum lines: {}\n\tnum files: {}\n\tpl delta: {}\n\tauthor delta: {}" + \
+            "\n\n[commit link]({})\n[pull request link]({})" + "_"*10 + "\n"
     
-    def message(self, commit_data:dict, text:str=None) -> None:
-        if text is None:
-            text = f"New commit at {commit_data['date']}:\n\tcommit author: {commit_data['author']}\n\tcommit hash: {commit_data['hash']}"
-
+    def message(self, text:str=None) -> None:
         if self.outer:
             self.outer.send_message(text)
         else:
             print(text)
 
-    def send_commits_info(self, commits:list, form:str=None) -> None:
+    def send_commits_info(self, commits:CommitData, form:str=None) -> None:
         if form is None:
             form = self.commit_info_form
         
-        if len(commits) > 10:
-            text = f"detected {len(commits)}. see cache date fro details"
+        if len(commits) > 8:
+            text = f"detected {len(commits)}. see history date fro details"
         else:
             text = 'Commit info.\n'
             for commit in commits:
-                text += form.format(commit['message'], commit['date'], commit['author'], commit['hash'], commit['url'], commit['pl_url'])
+                text += form.format(
+                    commit.message, 
+                    commit.date, 
+                    commit.author, 
+                    commit.hash, 
+                    commit.num_lines, 
+                    commit.num_files, 
+                    commit.delta_pl, 
+                    commit.delta_day, 
+                    commit.url, 
+                    commit.pl_url
+                    )
         
         if self.outer:
             self.outer.send_message(text)
@@ -76,13 +186,14 @@ class Messanger:
 
 class Tracker:
 
-    def __init__(self, token:str, repo_name:str, out=None, sleep:int=30) -> None:
+    def __init__(self, token:str, repo_name:str, out=None, sleep:int=30, history_dir='history') -> None:
         self.pler = github_api.PullRequester(token, repo_name)
         self.time_handler = TimeHandler()
         self.messanger = Messanger(out)
+        self.fcreator = FeatureCreator()
+        self.cache = Cache(history_dir)
 
         self.commits_count = 0
-        self.cache = []
         self.date = self.time_handler.get_current_date()
         self.history_dir = 'history'
         utils.mkdir(self.history_dir)
@@ -95,14 +206,12 @@ class Tracker:
         diff = count - self.commits_count
         return diff
 
-    def extruct_commit_data(self, commit:Commit) -> dict:
-        return  {
-            'date': commit.commit.author.date.isoformat(),
-            'author': commit.commit.author.name,
-            'hash': commit.commit.sha,
-            'message': commit.commit.message,
-            'url': commit.url
-        }
+    def get_prev_commit(self, el:int, commits:list) -> Commit:
+        next_el += el
+        if next_el >= len(commits):
+            return None
+        else:
+            return commits[next_el]
 
     def get_commits_from_pl(self, pl:PullRequest) ->list:
         out = []
@@ -119,9 +228,10 @@ class Tracker:
                 else: 
                     commit = pl_commits[el]
                     if commit.commit.author.date >= self.date:
-                        extructed_data = self.extruct_commit_data(commit)
-                        extructed_data['pl_url'] = pl.url
-                        out.append(extructed_data)
+                        prev_commit_pl = self.get_prev_commit(el, pl_commits)
+                        prev_date_author = self.cache.set_author_last_date(commit.commit.author.name)
+                        commit_data = self.fcreator.create_features(commit, prev_commit_pl, prev_date_author, pl.url)
+                        out.append(commit_data)
                         el += 1
                     else:
                         getting = False
@@ -134,21 +244,15 @@ class Tracker:
             commit_list += commits
         return commit_list
 
-    def save_cache(self) -> None:
-        save_path = os.path.join(self.history_dir, 'commits_' + self.date.strftime('%d-%m-%Y') + '.json')
-        utils.save_json(self.cache, save_path)
-        logger.info(f"cache saved in {save_path}")
-
     def track(self):
         logger.info(f"start tracking at {self.date}")
         while True:
             #check new day
-            out = self.time_handler.is_new_date(self.date)
-            if out:
-                self.save_cache()
-                self.cache = []
+            check = self.time_handler.is_new_date(self.date)
+            if check:
+                self.cache.save(self.date)
+                self.cache.clear()
                 self.date = self.time_handler.get_current_date()
-                logger.info("cache cleared!")
             #get data    
             pls = self.pler.get_pull_request_list(date=self.date)
             logger.info(f"get {len(pls)} pls")
@@ -159,24 +263,10 @@ class Tracker:
                 logger.info(f"get {len(commits)} commits")
                 if len(commits) > 0:
                     self.messanger.send_commits_info(commits)
-                    self.cache += commits
-                    self.save_cache()
+                    self.cache.get(commits)
+                    self.cache.save(self.date)
             time.sleep(self.sleep)
 
-    def tarck_commits(self): #to trash
-        while True:
-            date = self.update_date()
-            commits = self.hook_commits(date)
-            print(f'Количество коммитов за {date.date().strftime("%d:%m:%Y")}: {commits.totalCount}')
-            diff_count = self.check_diff_count(commits.totalCount)
-            if diff_count > 0:
-                self.commits_count = commits.totalCount
-                for commit in commits[:diff_count]:
-                    commit_data = self.extruct_commit_data(commit)
-                    self.message(commit_data)
-                    self.cache.append(commit_data)
-                    self.save_cache()
-            time.sleep(30)
 
 
 def let_hook(config:dict) -> None:
