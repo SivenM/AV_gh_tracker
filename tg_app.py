ba54a23337070ea4ee78dd14bb6bbe6f16be51b1
SivenM <aztek8877@gmail.com>
2024-08-21 15:32:11 +0300

    tg connection exception

--- tg_app.py
+++ tg_app.py
@@ -1,6 +1,7 @@
 import argparse
 import telebot
-
+from requests.exceptions import ConnectTimeout
+from loguru import logger
 
 class TgBot:
     def __init__(self, token:str, chat_id=str) -> None:
@@ -8,4 +9,7 @@ class TgBot:
         self.chat_id=chat_id
 
     def send_message(self, text:str):
-        self.bot.send_message(chat_id=self.chat_id, text=text, parse_mode='Markdown', disable_web_page_preview=True)
+        try:
+            self.bot.send_message(chat_id=self.chat_id, text=text, parse_mode='Markdown', disable_web_page_preview=True)
+        except ConnectTimeout:
+            logger.warning(f'connect to telegram lost')
